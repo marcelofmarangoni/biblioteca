5e24fe2e94f7ab2fe48fee8ed30a45077e34069e
Marcelo <marcelofmarangoni@gmail.com>
2024-11-20 12:49:21 -0300

    Criando a classe model e a classe repository

---
+++ src/main/java/com/marangoni/biblioteca/repository/AuthorRepository.java
@@ -0,0 +1,9 @@
+package com.marangoni.biblioteca.repository;
+
+import com.marangoni.biblioteca.model.Author;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AuthorRepository extends JpaRepository <Author, Long> {
+}
